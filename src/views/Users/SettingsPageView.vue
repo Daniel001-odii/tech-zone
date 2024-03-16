746b16ceaa4a423484e81e3d219fc8b69937cf18
Odii Daniel <xenithheight@gmail.com>
2024-03-16 02:07:46 +0100

    fix all settings page

--- src/views/Users/SettingsPageView.vue
+++ src/views/Users/SettingsPageView.vue
@@ -1,4 +1,13 @@
 <template>
+<div v-if="loading" class="text-center fixed top-0 bottom-0 w-screen flex justify-center items-center z-30 dark:bg-[rgb(0,0,0,70%)] bg-[rgba(255,255,255,0.7)]">
+    <div role="status">
+        <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
     <div>
         <TemplateView :leftNav="true">
             <template #page-title>Settings</template>
@@ -55,14 +64,14 @@
                             </div>
                             <div class="flex flex-col w-full mt-3">
                                 <label for="email">Phone Number</label>
-                                <input class="form_input" type="phone" name="phone" id="phone" :value="user.contact">
+                                <input v-if="user.profile" class="form_input" type="phone" name="phone" id="phone" v-model="user.profile.phone">
                             </div>
-                            <button class="btn mt-3">Update</button>
+                            <button class="btn mt-3" type="submit">Update</button>
                         </form>
 
-                        <form @submit.prevent="updateUserData" class="mt-3">
+                        <div @submit.prevent="updateUserData" class="mt-3">
                             <h1 class="font-bold mb-3 text-lg">Profile visibility</h1>
-                            settings: {{ user.settings }}
+                            <!-- settings: {{ user.settings }} -->
                             <div class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
                                 <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
@@ -70,16 +79,15 @@
                                 <span class="sr-only">Info</span>
                                 <div>In case of account inactivity your profile is automaitcally set private</div>
                             </div>
-                            
                             <div class="flex items-center mb-4">
-                                <input id="default-radio-1" type="radio" value="public" name="default-radio" v-model="settings.profile_visibility" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <input id="default-radio-1" type="radio" value="public" name="profile-visibility" @change="updateUserData" v-model="settings.profile_visibility"  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="default-radio-1" class="ms-2 text-lg  font-medium text-gray-900 dark:text-gray-300">Public</label>
                             </div>
                             <div class="flex items-center">
-                                <input id="default-radio-2" type="radio" value="private" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <input id="default-radio-2" type="radio" value="private" name="profile-visibility" @change="updateUserData" v-model="settings.profile_visibility"  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="default-radio-2" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">Private</label>
                             </div>
-                        </form>
+                        </div>
                         
                         <form class="">
                             <h1 class="font-bold mb-3 text-lg mt-3">Password & Security</h1>
@@ -104,7 +112,7 @@
                         </div>
                     </div>
 
-                    <form v-if="active_tab == 3" class="section mb-3">
+                    <form @submit.prevent="updateUserData" v-if="active_tab == 3" class="section mb-3">
                         <h1 class="font-bold mb-3 text-lg">Notification Settings</h1>
                         <div class=" flex flex-row items-center justify-between justify-center">
                             <div class="flex flex-col">
@@ -113,7 +121,7 @@
                             </div>
                             
                             <label class="inline-flex items-center cursor-pointer">
-                                <input type="checkbox" value="" class="sr-only peer" v-model="settings.notifications.contracts" checked>
+                                <input type="checkbox" value="" class="sr-only peer" @change="updateUserData" v-model="settings.notifications.contracts">
                                 <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                             </label>
                         </div>
@@ -125,7 +133,7 @@
                             </div>
                             
                             <label class="inline-flex items-center cursor-pointer">
-                                <input type="checkbox" value="" class="sr-only peer" v-model="settings.notifications.messages" checked>
+                                <input type="checkbox" value="" class="sr-only peer" @change="updateUserData" v-model="settings.notifications.messages">
                                 <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                             </label>
                         </div>
@@ -137,13 +145,13 @@
                             </div>
                             
                             <label class="inline-flex items-center cursor-pointer">
-                                <input type="checkbox" value="" class="sr-only peer" v-model="settings.notifications.emails" checked>
+                                <input type="checkbox" value="" class="sr-only peer" @change="updateUserData" v-model="settings.notifications.emails">
                                 <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                             </label>
                         </div>
                     </form>
 
-                    <div  v-if="active_tab == 4" class="section mb-3">
+                    <form @submit.prevent="updateUserData" v-if="active_tab == 4" class="section mb-3">
                         
                         <div class="  mt-3">
                             <h1 class="font-bold mb-3 text-lg">Withdrawal Information</h1>
@@ -156,21 +164,21 @@
                             </div>
                             <div class="flex flex-col max-w-sm">
                                 <label for="bank-name">Bank name</label>
-                                <input class="form_input" type="text" name="bank-name" id="bank_name" :value="settings.bank.name">
+                                <input class="form_input" type="text" name="bank-name" id="bank_name" v-model="settings.bank.name">
                             </div>
                             <div class="flex flex-col max-w-sm mt-5">
                                 <label for="sort-code">Bank Sort code</label>
-                                <input class="form_input" type="number" name="sort-code" id="sort_code" :value="settings.bank.sort_code">
+                                <input class="form_input" type="number" name="sort-code" id="sort_code" v-model="settings.bank.sort_code">
                             </div>
                             <div class="flex flex-col max-w-sm mt-5">
                                 <label for="account-number">Account number</label>
-                                <input class="form_input" type="number" name="account-number" id="account_number" :value="settings.bank.account_number">
+                                <input class="form_input" type="number" name="account-number" id="account_number" v-model="settings.bank.account_number">
                             </div>
                         </div>
                         <button class="btn mt-3 w-fit">Save bank info</button>
-                    </div>
+                    </form>
 
-                    <div  v-if="active_tab == 5" class="section mb-3">
+                    <form @submit.prevent="updateUserData"  v-if="active_tab == 5" class="section mb-3">
                         
                         <div class="mt-3">
                             <div class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
@@ -180,15 +188,33 @@
                                 <span class="sr-only">Info</span>
                                 <div>Your NIN number would be used for verifying your account once provided..</div>
                             </div>
-                            
+                            <!-- {{ settings.KYC }} -->
                             <h1 class="font-bold mb-3 text-lg">NIMC Information</h1>
-                            <div class="flex flex-col w-full">
+                            <div class=" ">
                                 <label for="nin-number">NIN Number</label>
-                                <input class="form_input" type="number" name="nin-number" id="nin-number" :value="user.email">
+                                <div class="flex flex-row items-center gap-3">
+                                    <div class="flex flex-col w-full">
+                                        <input class="form_input" type="number" name="nin-number" id="nin-number" v-model="settings.KYC.NIN_number" :disabled="settings.KYC.is_verified">
+                                    </div>
+                                    <svg v-if="settings.KYC.is_verified" class="w-6 h-6 text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z" clip-rule="evenodd"/>
+                                    </svg>
+                                    <svg v-else class="w-6 h-6 text-red-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z" clip-rule="evenodd"/>
+                                    </svg>
+                                </div>
+                                
+                               
+
                             </div>
-                            <button class="btn mt-3">Verify now</button>
+                            
+
+                            <button type="submit" class="btn mt-3" :disabled="settings.KYC.is_verified">
+                                <span v-if="settings.KYC.is_verified">Verified Successfully</span>
+                                <span v-else>Verify now</span>
+                            </button>
                         </div>
-                    </div>
+                    </form>
 
                     
                 </div>
@@ -207,14 +233,19 @@ export default {
     components: { TemplateView, ContractStatus },
     data(){
         return{
+            loading: false,
             contracts: '',
             headers: {
                 Authorization: `JWT ${localStorage.getItem('life-gaurd')}`
             },
             current_mode: '',
-            active_tab: 3,
+            active_tab: 1,
 
-            user: {},
+            user: {
+                profile:{
+                    phone: '',
+                }
+            },
             settings:{
                 bank: {
                     name: '',
@@ -230,7 +261,12 @@ export default {
                     contracts: true,
                     messages: true,
                     email: true,
-                }
+                },
+                KYC: {
+                    NIN_number: '',
+                    is_verified: true,
+                },
+                profile_visibility: '',
             },
         }
     },
@@ -265,22 +301,44 @@ export default {
         },
 
         async getUserData(){
+            this.loading = true;
             const headers = this.headers;
             try{    
                 const response = await axios.get(`${this.api_url}/user`, { headers });
                 // console.log(response)
-                this.user = response.data.user
-                this.settings = response.data.user.settings
+                this.user = response.data.user;
+                this.settings.profile_visibility = this.user.settings.profile_visibility
+                if(this.user.settings.bank){
+                    this.settings.bank = this.user.settings.bank
+                }
+                if(this.user.settings.card){
+                    this.settings.bank = this.user.settings.bank
+                }
+                if(this.user.settings.notifications){
+                    this.settings.notifications = this.user.settings.notifications
+                }
+                if(this.user.settings.KYC){
+                    this.settings.KYC = this.user.settings.KYC
+                }
+                this.loading = false;
             }catch(error){
-                console.log("error getting user details: ", error)
+                console.log("error getting user details: ", error);
+                this.loading = true;
             }
         },
 
         async updateUserData(){
+            this.loading = true;
             const headers = this.headers;
             try{
-                const response = await axios.patch(`${this.api_url}/user/profile`, this.user, { headers });
-                console.log(response)
+                const user = this.user;
+                user.settings = this.settings;
+
+                console.log(user)
+
+                const response = await axios.patch(`${this.api_url}/user/profile`, user, { headers });
+                console.log("set successful: ", response);
+                this.loading = false;
             }catch(error){
                 console.log("error updating user profile: ", error);
             }
