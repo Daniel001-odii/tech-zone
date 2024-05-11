0201b1b95a03c2376cad2226019ce6f5345985b8
Odii Daniel <xenithheight@gmail.com>
2024-05-11 11:51:23 +0100

    update to overview page

--- src/views/Users/OverviewPageView.vue
+++ src/views/Users/OverviewPageView.vue
@@ -1,5 +1,29 @@
 <template>
     <FullPageLoading v-if="loading"/>
+
+    <Modal :title="'Contract Review'" :modal_active="show_review_modal">
+        <template #body>
+          <div class="flex flex-row gap-3 w-96">
+            <img :src="employer.profile.image_url" class="h-10 w-10 rounded-full"/>
+            <div class="flex flex-col">
+                <p>{{ employer.company_name }}</p>
+                <div class="flex flex-col">
+                    <p class="inline-block mr-2 text-tz_blue" v-html="generateStarRatingFromInteger(feedback.rating)"></p>
+                    <span>{{ feedback.review }}</span>
+                </div>
+            </div>
+          </div>
+
+          
+
+            <!-- {{ employer }} <br/>  
+            {{ feedback }} -->
+           
+            
+        </template>
+        <template #footer></template>
+    </Modal>
+
     <!--HTML NAIRA SIGN--- &#8358; &#x20A6; -->
     <div class="h-full flex flex-col relative">
        <PageTitle>Overview & Earnings</PageTitle>
@@ -71,12 +95,6 @@
                         </thead>
                         <tbody>
                             <tr v-for="(contract, contract_id) in contracts" :key="contract_id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <!-- <td class="w-4 p-4">
-                                    <div class="flex items-center">
-                                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                    </div>
-                                </td> -->
                                 <td scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                                     <img class="h-10 w-10 rounded-full" :src="contract.employer.profile.image_url" alt="company image">
                                     <div class="ps-3">
@@ -93,32 +111,22 @@
                                     <span v-else>-</span>
                                 </td>
                                 <td class="px-6 py-4">
-                                <!-- <span v-if="user.profile.location">{{ user.profile.location.state }}</span> 
-                                <span v-else>not available</span> -->
                                 N0.00
                                 </td>
                                 <td class="px-6 py-4">
                                     <ContractStatus :type="`${contract.status}`"/>
-                                    <!-- <div class="flex items-center">
-                                        <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Active
-                                    </div> -->
                                 </td>
                                 <td class="px-6 py-4">
-                                    <button class="btn min-w-[180px]" v-if="contract.employer_feedback.rating > 0">open review <i class="bi bi-arrow-right ml-2"></i></button>
+                                    <button class="btn min-w-[180px]" v-if="contract.employer_feedback.rating > 0" @click="showContractReview(contract.employer, contract.user_feedback)">open review <i class="bi bi-arrow-right ml-2"></i></button>
                                     <span v-else>No review yet</span>
                                 </td>
                                 <td class="px-6 py-4 relative">
-                                    <!-- <button class="p-2 bg-red-500 text-white" @click="dropdown[contract_id] = !dropdown[contract_id]">menu: {{ dropdown[contract_id] }}</button>
-                                    <div v-if="dropdown[contract_id]" class="flex-col min-w-[150px] bg-white border p-3 rounded-md absolute right-5 group-hover:flex">
-                                        <span>View contract</span>
-                                        <span>Open taskwatch</span>
-                                    </div> -->
                                     <ActionDropwdown>
-                                        <RouterLink :to="`/in/contracts/${contract._id}`" class="hover:bg-slate-100 p-3">
+                                        <RouterLink :to="`/in/contracts/${contract._id}`" class="hover:bg-slate-100 dark:hover:bg-slate-600 p-3">
                                             <i class="bi bi-gift"></i> 
                                             View Contract
                                         </RouterLink>
-                                        <RouterLink :to="`/in/contracts/${contract._id}/watch`" class="hover:bg-slate-100 p-3">
+                                        <RouterLink :to="`/in/contracts/${contract._id}/watch`" class="hover:bg-slate-100 dark:hover:bg-slate-600 p-3">
                                             <i class="bi bi-clock-history"></i> 
                                             Open in Taskwatch
                                         </RouterLink>
@@ -148,11 +156,20 @@ import FullPageLoading from '@/components/FullPageLoading.vue';
 import { Dropdown } from 'flowbite';
 
 import ActionDropwdown from '@/components/ActionDropdown.vue';
+import Modal from '@/components/Modal.vue';
 
+import { generateStarRatingFromInteger } from '@/utils/ratingStars';
 
 export default {
     name: "OverviewPageView",
-    components: { TemplateView, ContractStatus, PageTitle, FullPageLoading, ActionDropwdown },
+    components: { 
+        TemplateView, 
+        ContractStatus, 
+        PageTitle, 
+        FullPageLoading, 
+        ActionDropwdown,
+        Modal,
+     },
     data(){
         return{
             contracts: '',
@@ -164,11 +181,21 @@ export default {
             loading: false,
 
             dropdown: [],
+            show_review_modal: false,
+
+            employer: '',
+            feedback: {
+                review: '',
+                rating: ''
+            },
+            generateStarRatingFromInteger,
         }
     },
     methods:{
-        closeContractMenu(index){
-            this.contract_menu[index] = false;
+        showContractReview(employer, employer_feedback){
+            this.feedback = employer_feedback;
+            this.employer = employer;
+            this.show_review_modal = !this.show_review_modal;
         },
         // get user...
         async getUser(){
